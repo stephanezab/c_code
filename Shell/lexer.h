ba2db341ea6ac2d9294a1da3b49f196fda4086a9
stephane zabayo <steph@DESKTOP-KP1QV4S>
2020-12-14 14:20:39 -0500

    commit2

--- Shell/lexer.h
+++
@@ -1,6 +0,0 @@
-struct yy_buffer_state;
-typedef struct yy_buffer_state *YY_BUFFER_STATE;
-YY_BUFFER_STATE yy_scan_string (const char *yy_str  );
-extern char *yytext;
-int yyparse (void);
-
--- shelljr/README.txt
+++
@@ -1,3 +0,0 @@
-To generate output files run your shell using the provided input
-For example: shell_jr < public01.in > my_results   
-Then compare my_results against the provided output.
